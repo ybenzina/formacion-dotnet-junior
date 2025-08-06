9012ab2776aac91915271bda647b12a01ff0d543
Driss <driss@example.com>
2025-08-06 13:45:58 +0200

    Implementación de la solución .NET con proyectos Core y Console

---
+++ Semana2/Ejercicio2/src/MyApp.Console/Program.cs
@@ -0,0 +1,7 @@
+﻿using MyApp.Core.Servicios;
+
+var servicio = new ServicioSaludo();
+Console.WriteLine("¿Cual es tu nombre?");
+var nombre = Console.ReadLine();
+Console.WriteLine(servicio.Saludar(nombre));
+
