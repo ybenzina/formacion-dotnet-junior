8ec5a454b519b4d49f66054d8dc0d043da59004e
Driss <driss@example.com>
2025-08-06 13:46:17 +0200

    Add initial project structure and implement ServicioSaludo class

---
+++ Semana2/Ejercicio2/src/MyApp.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace MyApp.Core;
+
+public class Class1
+{
+
+}
---
+++ Semana2/Ejercicio2/src/MyApp.Core/Servicios/ServicioSaludo.cs
@@ -0,0 +1,10 @@
+namespace MyApp.Core.Servicios
+{
+    public class ServicioSaludo
+    {
+        public string Saludar(string nombre)
+        {
+            return $"¡Hola, {nombre}!";
+        }
+    }
+}
